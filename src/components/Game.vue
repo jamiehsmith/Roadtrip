becadb10f1fea759f6229aa1ed7adb9da06df2b6
Jamie Smith <jamiesmith357@gmail.com>
2022-02-23 19:08:20 -0500

    Git init.

---
+++ src/components/Game.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    Here is a Unity game
+    <Unity />
+  </div>
+</template>
+
+<script>
+import Unity from 'vue-unity-webgl';
+
+export default {
+  name: 'Game',
+
+  components: {
+    Unity,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
