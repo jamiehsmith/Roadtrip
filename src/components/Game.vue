dfddc1e6ef853314d47bc6c0b1b8e97ba729db22
Jamie Smith <jamiesmith357@gmail.com>
2022-02-26 13:46:12 -0500

    Rendering Unity game, sending event to Unity.

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,35 +1,74 @@
 <template>
   <div>
-    Here is a Unity game
-    <Unity />
+    <div class="header">
+      <el-input
+        class="input"
+        :placeholder="isLoading ? 'Game loading...' : 'How many poptarts?'"
+        v-model="input"
+        :disabled="isLoading"
+        clearable
+      />
+      <el-button :disabled="!input" @click="startGame"
+        >Gimme poptarts</el-button
+      >
+    </div>
+    <br /><br />
+    <Unity :unity="unityContext" width="600px" height="400px" />
   </div>
 </template>
 
 <script>
-import Unity from 'vue-unity-webgl';
+import UnityWebgl from 'unity-webgl';
+
+const Unity = new UnityWebgl({
+  loaderUrl: 'build/game.loader.js',
+  dataUrl: 'build/game.data',
+  frameworkUrl: 'build/game.framework.js',
+  codeUrl: 'build/game.wasm',
+});
 
 export default {
   name: 'Game',
 
   components: {
-    Unity,
+    Unity: UnityWebgl.vueComponent,
   },
-}
+
+  data() {
+    return {
+      input: '',
+      isLoading: true,
+      unityContext: Unity,
+    };
+  },
+
+  methods: {
+    startGame() {
+      if (isNaN(this.input)) {
+        alert('That\'s not a number, bozo');
+        return;
+      }
+      const numSquares = parseInt(this.input);
+      // Find Unity method "CreateSquares" and passes the numSquares argument to it.
+      Unity.send('Gameplay', 'CreateSquares', numSquares);
+    },
+  },
+
+  created() {
+    Unity.on('progress', (percent) => console.log('Unity Loaded: ', percent))
+      .on('created', () => (this.isLoading = false))
+      .on('destroyed', () => console.log('Unity Instance: Destroyed.'));
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .input {
+    padding-right: 10px;
+  }
 }
 </style>
