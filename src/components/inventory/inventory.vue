a46727791b6a19443b5627499a2ab93265af1fe3
Jamie Smith <jamiesmith357@gmail.com>
2022-05-06 09:10:13 -0400

    Adding metric boxes component to inventory page.

--- src/components/inventory/inventory.vue
+++ src/components/inventory/inventory.vue
@@ -1,25 +1,74 @@
 <template>
+  <div>
     <TitleHeader title="Inventory" :userAddress="userAddress" />
+    <div class="metric-boxes">
+      <div v-for="box in metricBoxes" :key="box.index">
+        <MetricBox v-bind="box" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import TitleHeader from '../../components/shared/title-header';
+import MetricBox from '../../components/shared/metric-box';
 
 export default {
   name: 'Inventory',
 
   components: {
     TitleHeader,
+    MetricBox,
   },
 
-    props: {
+  props: {
     userAddress: {
       type: String,
       required: true,
     },
   },
 
+  computed: {
+    metricBoxes() {
+      return [
+        {
+          icon: 'fa-store',
+          title: 'New Airdrops',
+          number: 5,
+          color: '#F4B871',
+        },
+        {
+          icon: 'fa-store',
+          title: 'Tier Points',
+          number: 2,
+          color: '#FF6699',
+        },
+        {
+          icon: 'fa-store',
+          title: 'Car Upgrades',
+          number: 3,
+          color: '#71FACA',
+        },
+        {
+          icon: 'fa-store',
+          title: 'Total Police',
+          number: 4,
+        },
+        {
+          icon: 'fa-store',
+          title: 'Total Toll Workers',
+          number: 4,
+        },
+      ];
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.metric-boxes {
+  display: flex;
+  justify-content: space-around;
+  margin: 56px 0 74px 40px;
+}
+</style>
---
+++ src/components/shared/metric-box.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="metric-box-container" :style="{ 'background-color': color }">
+    <div>
+      <font-awesome-icon :icon="`fa-solid ${icon}`" />
+      <span class="title">{{ title }}</span>
+    </div>
+    <div class="number">
+      {{ number }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MetricBox',
+
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    number: {
+      type: Number,
+      default: 0,
+    },
+    color: {
+      type: String,
+      default: '#66B6FF',
+    },
+  },
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.metric-box-container {
+  height: 115px;
+  width: 174px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  font-weight: bold;
+  border-radius: 8px;
+  .title {
+    font-size: 16px;
+    text-align: left;
+  }
+  svg {
+    height: 25px;
+    width: 25px;
+  }
+  .title,
+  svg {
+    padding-top: 20px;
+  }
+  div {
+    padding-left: 20px;
+  }
+  div:first-of-type {
+    display: flex;
+    flex-direction: column;
+  }
+  div:last-of-type {
+    padding-right: 20px;
+  }
+  .number {
+    font-size: 48px;
+  }
+}
+</style>
